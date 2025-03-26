57c786b61424756370ae2c8341b2142eb5fcb742
Deba Rout <rdeba.ctc@gmail.com>
2025-03-26 23:16:29 +0530

    Initial version

---
+++ BasicTestForGit/HelloSelenium.java
@@ -0,0 +1,18 @@
+package BasicTestForGit;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class HelloSelenium
+{
+    @Test
+    public void openBrowser()
+    {
+       // WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver","F:/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com");
+
+    }
+}
